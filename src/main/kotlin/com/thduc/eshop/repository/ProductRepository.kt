74f7e3379828d5d6fd39acbe6b9334b868f9d523
nvngoc2409 <nvngoc2409@gmail.com>
2021-05-29 19:00:58 +0700

    change re

--- src/main/kotlin/com/thduc/eshop/repository/ProductRepository.kt
+++ src/main/kotlin/com/thduc/eshop/repository/ProductRepository.kt
@@ -21,12 +21,12 @@ interface ProductRepository: PagingAndSortingRepository<Product, Long> {
     fun findAllByNameContainingOrShop_NameContaining(name:String,sName:String,pageable: Pageable):Page<Product>
     @Query("SELECT p.* FROM product as p LEFT JOIN (SELECT * FROM recommend where user_id = :id ) as r " +
             "on p.id = r.product_id where p.status= 0 order by r.num desc"
-    ,nativeQuery = true,countQuery = "SELECT p FROM product as p where p.status= 0 ")
+    ,nativeQuery = true,countQuery = "SELECT p.* FROM product as p where p.status= 0 ")
     fun findAllRecommendByStatus(@Param("id") id: Long, pageable: Pageable): Page<Product>
 
     @Query("SELECT p.* FROM product as p LEFT JOIN (SELECT * FROM recommend where user_id = :id ) as r " +
             "on p.id = r.product_id where p.status= 0 and p.id not in(:exceptIds) order by r.num desc"
-        ,nativeQuery = true,countQuery = "SELECT p FROM product as p where p.id not in(:exceptIds) and p.status= 0 ")
+        ,nativeQuery = true,countQuery = "SELECT p.* FROM product as p where p.id not in(:exceptIds) and p.status= 0 ")
     fun findAllByStatusExcept(@Param("id") id: Long, @Param("exceptIds") exceptIds:List<Long>?, pageable: Pageable): Page<Product>
 
     @Query("SELECT c.id FROM Cart as c where c.user.id = :id")
@@ -34,7 +34,7 @@ interface ProductRepository: PagingAndSortingRepository<Product, Long> {
 
     @Query("SELECT p.* FROM product as p JOIN product_categories on product_categories.product_id = p.id LEFT JOIN (SELECT distinct product_id,num FROM recommend  ) as r " +
             "on p.id = r.product_id where p.status= 0 and p.id not in(:exceptIds) order by CASE product_categories.categories_id WHEN :categoryId THEN 1 ELSE 2 END  ,r.num desc"
-        ,nativeQuery = true,countQuery = "SELECT p FROM product as p where p.status = 0 and p.id not in(:exceptIds)")
+        ,nativeQuery = true,countQuery = "SELECT p.* FROM product as p where p.status = 0 and p.id not in(:exceptIds)")
     fun findAllByStatusExceptAndCategories(@Param("categoryId") categoryId: Long, @Param("exceptIds") exceptIds:List<Long>?, pageable: Pageable): Page<Product>
 
 //    override fun customize(
