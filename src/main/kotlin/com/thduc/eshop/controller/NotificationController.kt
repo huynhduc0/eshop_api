6ec0d5f2c8358282338fa169d65d2e626779da17
Trương Huỳnh Đức <38066181+huynhduc0@users.noreply.github.com>
2021-05-31 17:14:09 +0700

    Update NotificationController.kt

--- src/main/kotlin/com/thduc/eshop/controller/NotificationController.kt
+++ src/main/kotlin/com/thduc/eshop/controller/NotificationController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*
 import javax.websocket.server.PathParam
 
 @RestController
-@RequestMapping()
+@RequestMapping("notifications")
 class NotificationController(
     @Autowired val notificationService: NotificationService
 ) {
